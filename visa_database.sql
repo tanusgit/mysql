c0fa5e7935fd9a68bf729cdedc1817abea529386
Tanu <tanusree.debnath1990@gmail.com>
2017-01-24 15:23:58 -0600

    Update visa_database.sql

--- visa_database.sql
+++ visa_database.sql
@@ -27,3 +27,17 @@ update visa set employee_id= 1 where name='rim';
 select * from visa;
 update visa set employee_id=2 where address='2222ranch road';
 select * from visa;
+--show employee data whose name starts with A
+select * from tablename where columnname like 'A%';
+--show data where salary is greater than $4000
+select * from tablename where columnname >4000;
+--show data where salary is less than 4000
+select * from tablename where columnname <4000;
+--show data where salary is equal to 4000
+select * from tablename where columnname =4000;
+--show salary not equal to 4000
+select * from tablename where columnname !=4000;
+--show salary between 2000-4000 and gender is m
+select * from tablename where columnname >=2000 and columnname <=4000 and columnname='m';
+select * from tablename where columnname between 2000 and 4000 and gender='m';
+
